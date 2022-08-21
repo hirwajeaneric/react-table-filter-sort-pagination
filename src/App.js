c01657856ae2ae1fcf7428296534a2d628c2d8e9
hirwajeaneric <hirwajeaneric25@gmail.com>
2022-08-21 21:58:13 +0200

    Table capable of displaying data for now.

--- src/App.js
+++ src/App.js
@@ -21,11 +21,42 @@ function App() {
   return (
     <MDBContainer>
       <div style={{marginTop: '100px'}}>
-        <h2>Search, Filter, Sort and Pagination using JSON Fake Rest API</h2>
+        <h2 className='text-center'>
+          Search, Filter, Sort and Pagination using JSON Fake Rest API
+        </h2>
         <MDBRow>
           <MDBCol size="12">
             <MDBTable>
-              
+              <MDBTableHead dark>
+                <tr>
+                  <th score="col">No.</th>
+                  <th score="col">Name</th>
+                  <th score="col">Email</th>
+                  <th score="col">Phone</th>
+                  <th score="col">Address</th>
+                  <th score="col">Status</th>
+                </tr>
+              </MDBTableHead>
+              {data.length === 0 ? (
+                <MDBTableBody className='align-center mb-0'>
+                  <tr>
+                    <td colSpan={8} className="text-center mb-0">No Data Found</td>
+                  </tr>
+                </MDBTableBody>
+              ):(
+                data.map((item, index) =>(
+                  <MDBTableBody key={index}>
+                    <tr>
+                      <th scope='row'>{index+1}</th>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.phone}</td>
+                      <td>{item.address}</td>
+                      <td>{item.status}</td>
+                    </tr>
+                  </MDBTableBody>
+                ))
+              )}
             </MDBTable>
           </MDBCol>
         </MDBRow>
